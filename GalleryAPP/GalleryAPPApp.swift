e82f245c029fdf890cf8cd72d270dadcc88b8b93
Eugene Shapovalov <edev@evernetica.com>
2025-04-03 10:15:12 +0300

    Initial Commit

---
+++ GalleryAPP/GalleryAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAPPApp.swift
+//  GalleryAPP
+//
+//  Created by Eugene Shapovalov on 03.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct GalleryAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
